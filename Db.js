5e3ca339443bc198a08460ffc2b07c49843eca1c
Deepak4965 <dc1412592@gmail.com>
2025-08-21 15:19:53 -0800

    final

--- Db.js
+++ Db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config();
 //Define the MongoDB connection URL
 //const mongoURL = process.env.MONGODB_URL //Replace 'mydatabase' with your satabase name 
-const mongoURL =process.env.mongoDB_URL
+const mongoURL =process.env.mongoDB_URL  
 //set up MongoDB connection
 mongoose.connect(mongoURL, {
 
